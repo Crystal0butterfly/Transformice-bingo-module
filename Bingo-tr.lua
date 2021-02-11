15ca2050848a20cb57c93ef0a31edc57712b9fba
xd__ninguem <70050455+Crystal0butterfly@users.noreply.github.com>
2021-02-11 11:36:40 +0200

    Create Bingo-tr.lua

---
+++ Bingo-tr.lua
@@ -0,0 +1,50 @@
+lang.tr = {
+    cm = {
+        welcome     = "#bingo'ya hoş geldiniz!",
+        helpCmd     = "Yardım menüsünü açmak için %s yazın veya %s (%s) tuşuna tıklayın.",
+        langChosen  = "Seçilen Dil: %s",
+        langList    = "Mevcut Diller: %s",
+        langCmd     = "Bir dil seçmek için %s yazın.",
+        closeWin    = "%s için bir numara kaldı!",
+        skipGame    = "%s mevcut oyunu kapattı/geçti.",
+        gameStarted = "Yeni oyun başlıyor. Herkese iyi şanslar!",
+        colorPicked = "Tahtanızın rengi bir sonraki oyunda değiştirilecek.",
+        newPassword = "Yeni Şifre: %s",
+        pwDisabled  = "Şifre devre dışı bırakıldı.",
+        credits     = "Modülün Sahibi Tortuegreen#0000"
+    },
+    help = {
+        devise = "Şans seninle olsun!",
+        goal = "Modülün amacı",
+        rulesPart1 = "1 ve 90 arasında sayılar görünür. Eğer numarayı tahtanızda varsa üzerine tıklayın.",
+        rulesPart2 = Sadece 1, 2 ve 3 sıra dolduran ilk oyuncu kazanır! Oyun 3 satırı bitirdiğinizde durur (that is to say the full board)."
+    },
+    cmd = {
+        c1Name = "Fare",
+        c1Desc = "Bir numarayı kontrol et",
+        c2Desc = "Yardım menüsünü açar",
+        c3Desc = "Odanın şifresini ayarlayın/silin. Sadece kendi odanızda yapabilirsiniz #bingo0Name",
+        c4Desc = "Mevcut dilleri görün veya dilinizi değiştirin.",
+        c5Desc = "Mevcut oyunu atla. Sadece kendi odanızda yapabilirsiniz #bingo0Name"
+    },
+    word = {
+        Find   = "Bul:",
+        Target = "Hedef:",
+        Gifts  = "Ödüller",
+        Help   = "Yardım",
+        Color  = "Renk",
+        Line   = "%s satır",
+        Lines  = "%s satırlar",
+        _or    = "veya",
+        text   = "Metin"
+    },  
+    waitMsg    = "Bir oyun devam ediyor. Lütfen bir sonrakine katılabilmek için birkaç dakika bekleyin.",
+    reportBugs = "Lütfen tüm bug/raporları Tortuegreen#0000'a bildirin.",
+    newGame    = "%s saniye içinde yeni bir tur başlayacak ...",
+    released   = "Sayılar (%s/90): ",
+    quine1     = "%s 1 satırı tamamladı!",
+    quine2     = "%s 2 satırı tamamladı!",
+    quine3     = "%s 3 satırı tamamladı!",
+    closeArea  = "Kapat",
+    colorPick  = "Tahtanın Rengi",
+}
